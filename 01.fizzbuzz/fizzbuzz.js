14707cd5090c9e6a33bb5f329b0f61bdeb5c3e82
motohiro-mm <polarbear.dx@gmail.com>
2023-12-19 10:51:42 +0900

    fizzbuzz問題を作成

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,3 +1,11 @@
-for (let n = 1; n <= 20; n++){
-  console.log(n);
+for (let n = 1; n <= 20; n++) {
+  if (n % 15 == 0) {
+    console.log("FizzBuzz");
+  } else if (n % 3 == 0) {
+    console.log("Fizz");
+  } else if (n % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(n);
+  }
 }
