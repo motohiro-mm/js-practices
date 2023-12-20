82fd5ca11b3db4816704b759ef646bbb6ea5c292
motohiro-mm <polarbear.dx@gmail.com>
2023-12-20 19:22:22 +0900

    年月を指定有無で条件分岐を設定、月年を表示するようにした

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,9 +2,33 @@ import { default as minimist } from 'minimist';
 
 let argv = minimist(process.argv.slice(2));
 
-let first_day = new Date(argv.y,argv.m-1);
-let last_day = new Date(argv.y,argv.m,0);
+let today = new Date();
 
+function decide_month(params){
+  if (params.hasOwnProperty('m')){
+    return params.m;
+  }else{
+    return today.getMonth()+1;
+  }
+}
+
+function decide_year(params){
+  if (params.hasOwnProperty('y')){
+    return params.y;
+  }else{
+    return today.getFullYear();
+  }
+}
+
+//月と年の表示
+let target_month = decide_month(argv);
+let target_year = decide_year(argv);
+console.log(target_month + "月 " + target_year);
+
+let first_day = new Date(target_year,target_month-1);
+let last_day = new Date(target_year,target_month,0);
+
+//曜日の表示
 const days_of_week = ['日','月','火','水','木','金','土'];
 console.log(days_of_week.join(' '));
 
