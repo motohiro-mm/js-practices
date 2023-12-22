4cd9d3d02468ac8452811dd7e45c8f42a9637c57
motohiro-mm <polarbear.dx@gmail.com>
2023-12-22 18:48:50 +0900

    7日区切りで分けて表示するようにした

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,21 +1,21 @@
-import { default as minimist } from 'minimist';
+import { default as minimist } from "minimist";
 
 let argv = minimist(process.argv.slice(2));
 
 let today = new Date();
 
-function decide_month(params){
-  if (params.hasOwnProperty('m')){
+function decide_month(params) {
+  if (Object.prototype.hasOwnProperty.call(params, "m")) {
     return params.m;
-  }else{
-    return today.getMonth()+1;
+  } else {
+    return today.getMonth() + 1;
   }
 }
 
-function decide_year(params){
-  if (params.hasOwnProperty('y')){
+function decide_year(params) {
+  if (Object.prototype.hasOwnProperty.call(params, "y")) {
     return params.y;
-  }else{
+  } else {
     return today.getFullYear();
   }
 }
@@ -25,16 +25,25 @@ let target_month = decide_month(argv);
 let target_year = decide_year(argv);
 console.log(target_month + "月 " + target_year);
 
-let first_day = new Date(target_year,target_month-1);
-let last_day = new Date(target_year,target_month,0);
+let first_day = new Date(target_year, target_month - 1);
+let last_day = new Date(target_year, target_month, 0);
 
 //曜日の表示
-const days_of_week = ['日','月','火','水','木','金','土'];
-console.log(days_of_week.join(' '));
+const days_of_week = ["日", "月", "火", "水", "木", "金", "土"];
+console.log(days_of_week.join(" "));
 
-//console.log(first_day.toString());
-//console.log(last_day.toString());
-
-for ( let day = first_day.getDate(); day <= last_day.getDate(); day++){
-  console.log(day);
+let days = [];
+for (let day = first_day.getDate(); day <= last_day.getDate(); day++) {
+  days.push(day);
 }
+let week1 = days.slice(0,7);
+let week2 = days.slice(7,14);
+let week3 = days.slice(14,21);
+let week4 = days.slice(21,28);
+let week5 = days.slice(28);
+
+console.log(week1.join(' '));
+console.log(week2.join(' '));
+console.log(week3.join(' '));
+console.log(week4.join(' '));
+console.log(week5.join(' '));
