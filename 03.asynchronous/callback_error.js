e09e70ab31d96c2bb2b707feba4f99dabaa72528
motohiro-mm <polarbear.dx@gmail.com>
2024-01-04 08:35:53 +0900

    アロー関数で書き換え

--- 03.asynchronous/callback_error.js
+++ 03.asynchronous/callback_error.js
@@ -2,28 +2,20 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  function () {
-    db.run(
-      "insert into abooks(title) values(?)",
-      "リーダブルコード",
-      function (err) {
-        if (err) console.log(`1つ目登録時エラー:${err.message}`);
-        db.run(
-          "insert into books(id) values(?)",
-          "パーフェクトRuby on Rails",
-          function (err) {
-            if (err) console.log(`2つ目登録時エラー:${err.message}`);
-            db.each("select * from bbooks", function (err, row) {
-              if (err) {
-                console.log(`取得時エラー:${err.message}`);
-              } else {
-                console.log(`${row.id} : ${row.title}`);
-              }
-            });
-          },
-        );
-      },
-    );
+  () => {
+    db.run("insert into abooks(title) values(?)", "リーダブルコード", (err) => {
+      if (err) console.log(`1つ目登録時エラー: ${err.message}`);
+      db.run("insert into books(id) values(?)", "プログラマー脳", (err) => {
+        if (err) console.log(`2つ目登録時エラー: ${err.message}`);
+        db.each("select * from boooks", (err, row) => {
+          if (err) {
+            console.log(`取得時エラー: ${err.message}`);
+          } else {
+            console.log(`${row.id} : ${row.title}`);
+          }
+        });
+      });
+    });
   },
 );
 db.close();
