1ee6dc737da0490bbaa2a01e24e0cf86559d6bea
motohiro-mm <polarbear.dx@gmail.com>
2024-01-08 18:20:58 +0900

    callback_no_error.jsの必要ないエラーなしプログラムでのエラーハンドリング部分を削除

--- 03.asynchronous/callback_no_error.js
+++ 03.asynchronous/callback_no_error.js
@@ -3,18 +3,12 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
-    db.run("insert into books(title) values(?)", "リーダブルコード", (err) => {
-      if (err) console.log(`1つ目登録時エラー: ${err.message}`);
-      db.run("insert into books(title) values(?)", "プログラマー脳", (err) => {
-        if (err) console.log(`2つ目登録時エラー: ${err.message}`);
+    db.run("insert into books(title) values(?)", "リーダブルコード", () => {
+      db.run("insert into books(title) values(?)", "プログラマー脳", () => {
         db.all("select * from books", (err, rows) => {
-          if (err) {
-            console.log(`取得時エラー: ${err.message}`);
-          } else {
-            rows.forEach((row) => {
-              console.log(`${row.id} : ${row.title}`);
-            });
-          }
+          rows.forEach((row) => {
+            console.log(`${row.id} : ${row.title}`);
+          });
           db.close();
         });
       });
